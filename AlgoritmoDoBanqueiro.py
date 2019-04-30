86af21153e8f015fb3ac490e52f8c9f327e7360d
unknown <nelsondiasdemedeiros@gmail.com>
2019-04-30 00:08:12 -0300

    Falta decrementar de recursos disponiveis o recurso solicitado

--- AlgoritmoDoBanqueiro.py
+++ AlgoritmoDoBanqueiro.py
@@ -19,26 +19,48 @@ class AlgoritmoDoBanqueiro:
             nomeRecurso = input("\ndigite o nome do recurso [{}]: ".format(recurso+1))
             quantidadeRecurso = int(input("Digite a quantidade recurso: "))
             self.adicionarResursosExistentes(nomeRecurso, quantidadeRecurso)
+
         self.recursosDisponiveis = list(self.recursosExistentes)
         self.criandoMatrizDeAlocacaoCorrente()
-        print(self.recursosExistentes)
+        self.controllerProcessosRecursos()
+        #print(self.recursosExistentes)
 
     def criandoMatrizDeAlocacaoCorrente(self):
-        qntProcessos = int(input("Digite a quantidade de processos: "))
+        qntProcessos = int(input("\nDigite a quantidade de processos: "))
         self.quantidadeDeProcesos = qntProcessos
         for processo in range(self.quantidadeDeProcesos):
             self.adicionarRecursosProcesso('p'+str(processo+1))
-        print(self.processos)
+        #print(self.processos)
 
-    def cotrollerProcessosRecursos(self):
-    	for processo in range(len(self.processo)):
-    		pass
+    def controllerProcessosRecursos(self):
+        for processo in range(len(self.processos)):
+            for recurso in range(len(self.recursosExistentes)):
+                verifica = False
+                while(verifica != True):
+                    processoImprimir = self.processos[processo].copy().popitem()[0]
+                    recursoImprimir = self.recursosExistentes[recurso].copy().popitem()[0]
 
+                    recursoSolicitado = int(input("Digite a quantidade de recurso [{}] do [{}]: ".format(recursoImprimir, processoImprimir)))
+                    print("\nprocesso: {}\nrecurso: {}".format(self.recursosExistentes[recurso].copy().popitem()[0], self.processos[processo].copy().popitem()[1][recurso].copy().popitem()[0]))
+                    recursoExistente = self.recursosExistentes[recurso].copy().popitem()[0]
+                    processoRecurso = self.processos[processo].copy().popitem()[1][recurso].copy().popitem()[0]
+                    if recursoExistente == processoRecurso:
+                        print("recursos iguais")
+                        print(self.recursosDisponiveis[recurso].copy().popitem()[1])
+                        if recursoSolicitado <= self.recursosDisponiveis[recurso].copy().popitem()[1]:
+                            self.processos[processo][processoImprimir][recurso][recursoImprimir] = recursoSolicitado
+                            verifica = True
+                            print("RECURSO DISPONIVEL")
+                            print(self.processos)
+                        else:
+                            if self.recursosDisponiveis[recurso].copy().popitem()[1] == 0:
+                                self.processos[processo][processoImprimir][recurso][recursoImprimir] = 0
+                                verifica = True
+    '''
+    Falta decrementar em self.recursosDisponiveis o recursoSolicitado.
 
-
-
-
-
+    
+    '''
 
 
 if __name__ == '__main__':
