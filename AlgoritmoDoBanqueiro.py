e4cb32f60ad8b2f69f59f7e5e2311a6ed7dad64f
Meu Nome <danielavieira6689@gmail.com>
2019-05-24 14:52:50 -0300

    Implementação de processo de requisicao, não esta finalizado falta implmentar a analise do deadlock

--- AlgoritmoDoBanqueiro.py
+++ AlgoritmoDoBanqueiro.py
@@ -6,14 +6,19 @@ class AlgoritmoDoBanqueiro:
     def __init__(self):
         self.recursosDisponiveis = None
         self.recursosExistentes = []
-        self.processos = []
+        self.processosAlocacaoCorrente = []
+        self.processoDeRequisicoes = []
         self.cor = Cor()
 
     def adicionarResursosExistentes(self, nomeRecurso, qntRecurso):
         self.recursosExistentes.append({'{}'.format(nomeRecurso): qntRecurso})
 
-    def adicionarRecursosProcesso(self, nomeProcesso):
-        self.processos.append({'{}'.format(nomeProcesso): deepcopy(self.recursosExistentes)})
+    def adicionarRecursosProcessoAlocacaoCorrente(self, nomeProcesso):
+        self.processosAlocacaoCorrente.append({'{}'.format(nomeProcesso): deepcopy(self.recursosExistentes)})
+        self.adicionarRecursosProcessoDeRequisicoes(nomeProcesso)
+
+    def adicionarRecursosProcessoDeRequisicoes(self, nomeProcesso):
+        self.processoDeRequisicoes.append([{'{}'.format(nomeProcesso): deepcopy(self.recursosExistentes)}, {'executado': False}])
 
     def imprimirMatriz(self, lista, nome, cor):
         print("\n-- {} --".format(self.cor.colorir(nome, 'branco')))
@@ -33,40 +38,59 @@ class AlgoritmoDoBanqueiro:
         self.imprimirMatriz(self.recursosDisponiveis, "RECURSOS DISPONIVEIS", 'azul')
         self.criandoMatrizDeAlocacaoCorrente()
         self.controllerProcessosRecursos()
+        self.controllerProcessosDeRequisicao()
 
     def criandoMatrizDeAlocacaoCorrente(self):
         qntProcessos = int(input("\nDigite a quantidade de processos: "))
         for processo in range(qntProcessos):
-            self.adicionarRecursosProcesso('p'+str(processo+1))
+            self.adicionarRecursosProcessoAlocacaoCorrente('p' + str(processo + 1))
+
+    def controllerProcessosDeRequisicao(self):
+        for processo in range(len(self.processoDeRequisicoes)):
+            for recurso in range(len(self.recursosExistentes)):
+                keyProcessoAlocacaoCorrente = self.processosAlocacaoCorrente[processo].copy().popitem()[0]
+                keyRecursoAlocacaoCorrente = self.recursosExistentes[recurso].copy().popitem()[0]
+
+                keyProcessoDeRequisicao = self.processoDeRequisicoes[processo][0].copy().popitem()[0]
+                keyRecursoDeRequisicao = self.processoDeRequisicoes[processo][0][keyProcessoDeRequisicao][recurso].copy().popitem()[0]
+
+                recursoSolicitado = int(input("\nDigite a quantidade de recurso do processo de requisiçao[{}] do [{}]: ".format(self.cor.colorir(keyRecursoDeRequisicao, 'azul'), self.cor.colorir(keyProcessoDeRequisicao, 'verde'))))
+
+                if keyProcessoAlocacaoCorrente == keyProcessoDeRequisicao and keyRecursoAlocacaoCorrente == keyRecursoDeRequisicao:
+                    self.processoDeRequisicoes[processo][0][keyProcessoDeRequisicao][recurso][keyRecursoDeRequisicao] = recursoSolicitado
+
+        print("\n{}".format(self.processoDeRequisicoes))
 
     def controllerProcessosRecursos(self):
-        for processo in range(len(self.processos)):
+        for processo in range(len(self.processosAlocacaoCorrente)):
             for recurso in range(len(self.recursosExistentes)):
+
                 verifica = False
                 while(verifica != True):
-                    processoImprimir = self.processos[processo].copy().popitem()[0]
+                    processoImprimir = self.processosAlocacaoCorrente[processo].copy().popitem()[0]
                     recursoImprimir = self.recursosExistentes[recurso].copy().popitem()[0]
 
                     self.imprimirMatriz(self.recursosDisponiveis, "RECURSOS DISPONIVEIS", 'azul')
-                    recursoSolicitado = int(input("\nDigite a quantidade de recurso [{}] do [{}]: ".format(self.cor.colorir(recursoImprimir, 'azul'), self.cor.colorir(processoImprimir, 'verde'))))
+                    recursoSolicitado = int(input("\nDigite a quantidade de recurso do processo de alocaçao corrente [{}] do [{}]: ".format(self.cor.colorir(recursoImprimir, 'azul'), self.cor.colorir(processoImprimir, 'verde'))))
 
                     recursoExistente = self.recursosExistentes[recurso].copy().popitem()[0]
-                    processoRecurso = self.processos[processo].copy().popitem()[1][recurso].copy().popitem()[0]
+                    processoRecurso = self.processosAlocacaoCorrente[processo].copy().popitem()[1][recurso].copy().popitem()[0]
                     if recursoExistente == processoRecurso:
                         if recursoSolicitado <= self.recursosDisponiveis[recurso].copy().popitem()[1]:
-                            self.processos[processo][processoImprimir][recurso][recursoImprimir] = recursoSolicitado
+                            self.processosAlocacaoCorrente[processo][processoImprimir][recurso][recursoImprimir] = recursoSolicitado
                             self.recursosDisponiveis[recurso][recursoImprimir] = self.recursosDisponiveis[recurso][recursoImprimir] - recursoSolicitado
                             verifica = True
 
                         elif self.recursosDisponiveis[recurso].copy().popitem()[1] == 0:
-                            self.processos[processo][processoImprimir][recurso][recursoImprimir] = 0
+                            self.processosAlocacaoCorrente[processo][processoImprimir][recurso][recursoImprimir] = 0
                             verifica = True
 
                         else:
                             print("\nQuantidade de recurso disponivel em [{}] do [{}] => {}".format(self.cor.colorir(recursoImprimir, 'azul'), self.cor.colorir(processoImprimir, 'verde'), self.recursosDisponiveis[recurso].copy().popitem()[1]))
 
-        self.imprimirMatriz(self.processos, "PROCESSOS RECURSOS", 'verde')
+        self.imprimirMatriz(self.processosAlocacaoCorrente, "PROCESSOS RECURSOS", 'verde')
         self.imprimirMatriz(self.recursosDisponiveis, "RECURSOS DISPONIVEIS", 'azul')
+
 
 
 if __name__ == '__main__':
